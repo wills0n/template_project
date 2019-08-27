b0b04c1065efd2d40449544113e57a69d451d23f
wills0n <123@123.com>
2019-08-27 11:46:47 -0300

    Added install_ruby.sh script

---
+++ scripts/install_ruby.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install gnupg2
+
+echo "Start install Ruby"
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+ruby -v
+gem -v bundler
+echo "End install Ruby"
