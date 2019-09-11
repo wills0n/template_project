c2e6ab460e0c6df9ad0cf79e110cb78891446785
wills0n <p140kp102rus@gmail.com>
2019-09-11 14:44:04 -0300

    add ubuntu.18.json

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -7,8 +7,8 @@ sudo apt install -y ruby-full ruby-bundler build-essential
 echo "End install Ruby"
 
 echo "Start install MongoDB"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo bash -c 'echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
