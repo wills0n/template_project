0ec6165975c4585b1220691833dfc853f212ef78
wills0n <p140kp102rus@gmail.com>
2019-09-14 16:47:12 +0300

    Fixed startup_script.sh

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
 sudo apt-get update
+sudo apt-get install gnupg2
 
 echo "Start install Ruby"
-sudo apt-get update
-sudo apt install -y ruby-full ruby-bundler build-essential
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.1
+rvm use 2.4.1 --default
+gem install bundler -V --no-ri --no-rdoc
+ruby -v
+bundler -v
 echo "End install Ruby"
 
 echo "Start install MongoDB"
