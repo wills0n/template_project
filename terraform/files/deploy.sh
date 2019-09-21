953a3e59c0b2c121eaab7b68616991a25a39171a
wills0n <p140kp102rus@gmail.com>
2019-09-21 02:45:41 -0700

    Added last work version branch terraform1

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
